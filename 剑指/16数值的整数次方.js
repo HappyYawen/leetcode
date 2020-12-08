ff21f7c7d566498be45deba6c6ca0b4048779c8e
karla.guo <karla.guo@ucloud.cn>
2020-12-08 14:56:40 +0800

    16数值的整数次方

---
+++ 剑指/16数值的整数次方.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ * 时间复杂度O(log(n)以2为底)
+ */
+var myPow = function(x, n) {
+    if(n === 0) {
+        return 1
+    }
+    let res = 1.0, isMinus = false
+    if(n < 0) {
+        isMinus = true
+        n = -n
+    }
+    while(n != 0) {
+        if(n % 2 !== 0) {
+            res *= x
+            n--
+        }
+        x *= x
+        n >>>= 1 //无符号向右移动
+        // 或者可以
+        // n = n/2
+    }
+
+    return isMinus ? 1 / res : res
+};
