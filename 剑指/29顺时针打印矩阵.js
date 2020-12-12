b16efbd73ea91523e01e261c7f00cff8a426b599
karla.guo <karla.guo@ucloud.cn>
2020-12-12 21:01:42 +0800

    29顺时针打印矩阵

---
+++ 剑指/29顺时针打印矩阵.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+    if(!matrix || !matrix[0]) {
+        return []
+    }
+    let r = matrix.length, c = matrix[0].length
+    let res = []
+    //cycle记录圈数,nr与nc是当前的行总数、列总数，每一轮循环后，行数与列数都会减2
+    let cycle = 0, nr = r, nc = c
+    while(res.length < r*c) {
+        res = res.concat(matrix[cycle].slice(cycle, nc))
+        if(res.length < r*c){
+            for(let i = cycle+1; i<nr; i++) {
+                res.push(matrix[i][nc-1])
+            }
+        }
+        if(nr > 1 && res.length < r*c) {
+            for(let i = nc-2; i >= cycle; i-- ) {
+                res.push(matrix[nr-1][i])
+            }
+        }
+        if(nc > 1 && res.length < r*c) {
+            for(let i = nr-2; i > cycle; i--) {
+                res.push(matrix[i][cycle])
+            }
+        }
+        cycle++
+        nr--
+        nc--
+    }
+    return res
+};
