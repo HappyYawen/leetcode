1b5a5b2d6652a3f261571d5d571d8ada3927ff31
karla.guo <karla.guo@ucloud.cn>
2020-12-23 22:24:22 +0800

    387.字符串中的第一个唯一字符

---
+++ 387.字符串中的第一个唯一字符.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    let map = new Map()
+    for(let i = 0; i< s.length; i++) {
+        if(map.has(s[i])) {
+            map.set(s[i], -1)
+        } else {
+            map.set(s[i], i)
+        }
+    }
+    for(let [key ,value] of map) {
+        if(value !== -1) {
+            return value
+        }
+    }
+    return -1
+};
