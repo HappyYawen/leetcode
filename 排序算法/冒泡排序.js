346b8ceb73b52b594e2e94af31a839c778d57566
karla.guo <karla.guo@ucloud.cn>
2021-01-09 17:27:31 +0800

    冒泡排序优化

--- 排序算法/冒泡排序.js
+++ 排序算法/冒泡排序.js
@@ -18,21 +18,28 @@ function bubbleSort(num) {
 }
 //优化
 function bubbleSort1(num) {
+    //最后一次交换元素的索引
+    let lastExchangeIndex = 0
+    //比较元素的右边界
+    let sortBorder = num.length - 1
     for (let i = 0; i < num.length - 1; i++) {
         //标记flag
         let flag = false
-        for (let j = 1; j < num.length - i; j++) {
+        for (let j = 1; j < sortBorder; j++) {
             if (num[j - 1] > num[j]) {
                 let temp = num[j - 1]
                 num[j - 1] = num[j]
                 num[j] = temp
                 flag = true
+                //最后一次交换元素的位置，说明后面的元素都已经排好序的，不需要重复比较了
+                lastExchangeIndex = j
             }
         }
         //如果没有移动，说明已经排好序了，直接跳出循环
         if (!flag) {
             break;
         }
+        sortBorder = lastExchangeIndex
     }
     console.log(num)
     return num
