3bf2d94fa15380eb1946963e0981dac0d20bc720
karla.guo <karla.guo@ucloud.cn>
2021-01-13 22:39:51 +0800

    703.数据流中的第K大元素

---
+++ 703.数据流中的第K大元素.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ * 不是最佳实践，最佳实践是用最大堆
+ */
+var KthLargest = function(k, nums) {
+    this.k = k
+    this.nums = nums.sort((a,b) => b-a)
+};
+
+/** 
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+    this.nums.push(val)
+    this.nums.sort((a,b) => b-a)
+    return this.nums[this.k-1]
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
